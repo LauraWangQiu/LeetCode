875d3a66152236312a8d55a1a36535965328fd2f
LauraWangQiu <yiwang03@ucm.es>
2025-02-13 09:38:17 +0100

    605. Can Place Flowers

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 //#define _1768
 //#define _1071
 //#define _1431
+//#define _605
 
 #pragma region 1768. Merge Strings Alternately
 /*
@@ -199,6 +200,58 @@ int main(int argc, char** argv) {
     for (int i = 1; i < result.size(); ++i)
         cout << ", " << BOOL_STR(result[i]);
     cout << " ]\n";
+    return 0;
+}
+#endif
+#pragma endregion
+
+#pragma region 605. Can Place Flowers
+/*
+* You have a long flowerbed in which some of the plots are planted, and some are not. However, flowers cannot be planted in adjacent plots.
+* 
+* Given an integer array flowerbed containing 0's and 1's, where 0 means empty and 1 means not empty, and an integer n, return true if n new flowers can be planted in the flowerbed without violating the no-adjacent-flowers rule and false otherwise.
+* 
+* Example 1:
+* 
+* Input: flowerbed = [1,0,0,0,1], n = 1
+* Output: true
+* 
+* Example 2:
+* 
+* Input: flowerbed = [1,0,0,0,1], n = 2
+* Output: false
+* 
+* Constraints:
+* 
+*     1 <= flowerbed.length <= 2 * 104
+*     flowerbed[i] is 0 or 1.
+*     There are no two adjacent flowers in flowerbed.
+*     0 <= n <= flowerbed.length
+* 
+* https://leetcode.com/problems/can-place-flowers/solutions/6416870/easy-solution-advance-index-position-for-more-efficient-calculation
+*/
+
+// Time complexity: O(n), n = size of flowerbed
+// Space complexity: O(1)
+bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+    for (int i = 0; i < flowerbed.size();) {
+        if (!flowerbed[i]) {
+            if (i + 1 < flowerbed.size() && flowerbed[i + 1]) i += 3;
+            else if (i - 1 >= 0 && flowerbed[i - 1]) i++;
+            else if (--n <= 0) return true;
+            else i += 2;
+        } else i += 2;
+    }
+    return n <= 0;
+}
+
+#ifdef _605
+int main(int argc, char** argv) {
+    int f; cin >> f;
+    vector<int> flowerbed(f);
+    for (int& e : flowerbed) cin >> e;
+    int n; cin >> n;
+    cout << ((canPlaceFlowers(flowerbed, n)) ? "true" : "false") << '\n';
     return 0;
 }
 #endif
