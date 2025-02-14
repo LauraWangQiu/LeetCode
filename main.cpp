a777e34d68d0c466f8a00b76946bad401506faf7
LauraWangQiu <yiwang03@ucm.es>
2025-02-14 22:24:40 +0100

    334. Increasing Triplet Subsequence

--- main.cpp
+++ main.cpp
@@ -11,6 +11,7 @@ using namespace std;
 //#define _345
 //#define _151
 //#define _238
+//#define _334
 
 #pragma region 1768. Merge Strings Alternately
 /*
@@ -476,6 +477,60 @@ int main(int argc, char** argv) {
         cout << " ]\n";
     }
 
+    return 0;
+}
+#endif
+#pragma endregion
+
+#pragma region 334. Increasing Triplet Subsequence
+/*
+* Given an integer array nums, return true if there exists a triple of indices (i, j, k) such that i < j < k and nums[i] < nums[j] < nums[k]. If no such indices exists, return false.
+* 
+* Example 1:
+* 
+* Input: nums = [1,2,3,4,5]
+* Output: true
+* Explanation: Any triplet where i < j < k is valid.
+* 
+* Example 2:
+* 
+* Input: nums = [5,4,3,2,1]
+* Output: false
+* Explanation: No triplet exists.
+* 
+* Example 3:
+* 
+* Input: nums = [2,1,5,0,4,6]
+* Output: true
+* Explanation: The triplet (3, 4, 5) is valid because nums[3] == 0 < nums[4] == 4 < nums[5] == 6.
+* 
+* Constraints:
+* 
+*     1 <= nums.length <= 5 * 105
+*     -231 <= nums[i] <= 231 - 1
+* 
+* Follow up: Could you implement a solution that runs in O(n) time complexity and O(1) space complexity?
+*/
+
+// Time complexity: O(n), n = length of nums
+// Space complexity: O(n), n = length of nums
+bool increasingTriplet(vector<int>& nums) {
+    int first = INT_MAX, second = INT_MAX;
+    for (int n : nums) {
+        if (n <= first) first = n;
+        else if (n <= second) second = n;
+        else return true;
+    } return false;
+}
+
+#ifdef _334
+int main(int argc, char** argv) {
+    while (true) {
+        int n; cin >> n;
+        vector<int> input(n);
+        for (int& e : input) cin >> e;
+        cout << ((increasingTriplet(input)) ? "true" : "false") << '\n';
+    }
     return 0;
 }
 #endif
