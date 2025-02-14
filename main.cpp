f92b95bca5d3a62263f042f2f316aa2f66c852b1
LauraWangQiu <yiwang03@ucm.es>
2025-02-14 20:42:59 +0100

    238. Product of Array Except Self

--- main.cpp
+++ main.cpp
@@ -10,6 +10,7 @@ using namespace std;
 //#define _605
 //#define _345
 //#define _151
+//#define _238
 
 #pragma region 1768. Merge Strings Alternately
 /*
@@ -410,6 +411,71 @@ int main(int argc, char** argv) {
         string input; getline(cin, input);
         cout << reverseWords(input) << '\n';
     }
+    return 0;
+}
+#endif
+#pragma endregion
+
+#pragma region 238. Product of Array Except Self
+/*
+* Given an integer array nums, return an array answer such that answer[i] is equal to the product of all the elements of nums except nums[i].
+* 
+* The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+* 
+* You must write an algorithm that runs in O(n) time and without using the division operation.
+* 
+* Example 1:
+* 
+* Input: nums = [1,2,3,4]
+* Output: [24,12,8,6]
+* 
+* Example 2:
+* 
+* Input: nums = [-1,1,0,-3,3]
+* Output: [0,0,9,0,0]
+* 
+* Constraints:
+* 
+*     2 <= nums.length <= 105
+*     -30 <= nums[i] <= 30
+*     The input is generated such that answer[i] is guaranteed to fit in a 32-bit integer.
+* 
+* Follow up: Can you solve the problem in O(1) extra space complexity? (The output array does not count as extra space for space complexity analysis.)
+*/
+
+// Time complexity: O(n), n = length of nums
+// Space complexity: O(n), n = length of nums
+vector<int> productExceptSelf(vector<int>& nums) {
+    vector<int> output(nums.size(), 1);
+
+    int left = nums[0];
+    for (int i = 1; i < nums.size(); i++) {
+        output[i] *= left;
+        left *= nums[i];
+    }
+
+    int right = nums[nums.size() - 1];
+    for (int i = nums.size() - 2; i >= 0; i--) {
+        output[i] *= right;
+        right *= nums[i];
+    }
+
+    return output;
+}
+
+#ifdef _238
+int main(int argc, char** argv) {
+    while (true) {
+        int n; cin >> n;
+        vector<int> nums(n);
+        for (int& e : nums) cin >> e;
+        vector<int> answer = productExceptSelf(nums);
+        cout << "[ " << answer[0];
+        for (int i = 1; i < answer.size(); ++i)
+            cout << ", " << answer[i];
+        cout << " ]\n";
+    }
+
     return 0;
 }
 #endif
