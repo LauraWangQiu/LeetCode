e7963a76efd0b26b1fd279da9cf7baf9a371aa1a
LauraWangQiu <yiwang03@ucm.es>
2025-02-15 13:28:19 +0100

    443. String Compression

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ using namespace std;
 //#define _151
 //#define _238
 //#define _334
+//#define _443
 
 #pragma region 1768. Merge Strings Alternately
 /*
@@ -530,6 +531,80 @@ int main(int argc, char** argv) {
         vector<int> input(n);
         for (int& e : input) cin >> e;
         cout << ((increasingTriplet(input)) ? "true" : "false") << '\n';
+    }
+    return 0;
+}
+#endif
+#pragma endregion
+
+#pragma region 443. String Compression
+/*
+* Given an array of characters chars, compress it using the following algorithm:
+* 
+* Begin with an empty string s. For each group of consecutive repeating characters in chars:
+* 
+*     If the group's length is 1, append the character to s.
+*     Otherwise, append the character followed by the group's length.
+* 
+* The compressed string s should not be returned separately, but instead, be stored in the input character array chars. Note that group lengths that are 10 or longer will be split into multiple characters in chars.
+* 
+* After you are done modifying the input array, return the new length of the array.
+* 
+* You must write an algorithm that uses only constant extra space.
+* 
+* Example 1:
+* 
+* Input: chars = ["a","a","b","b","c","c","c"]
+* Output: Return 6, and the first 6 characters of the input array should be: ["a","2","b","2","c","3"]
+* Explanation: The groups are "aa", "bb", and "ccc". This compresses to "a2b2c3".
+* 
+* Example 2:
+* 
+* Input: chars = ["a"]
+* Output: Return 1, and the first character of the input array should be: ["a"]
+* Explanation: The only group is "a", which remains uncompressed since it's a single character.
+* 
+* Example 3:
+* 
+* Input: chars = ["a","b","b","b","b","b","b","b","b","b","b","b","b"]
+* Output: Return 4, and the first 4 characters of the input array should be: ["a","b","1","2"].
+* Explanation: The groups are "a" and "bbbbbbbbbbbb". This compresses to "ab12".
+* 
+* Constraints:
+* 
+*     1 <= chars.length <= 2000
+*     chars[i] is a lowercase English letter, uppercase English letter, digit, or symbol.
+*/
+
+// Time complexity: O(n), n = length of char vector
+// Space complexity: O(1)
+int compress(vector<char>& chars) {
+    int charsIdx = 0;
+    for (int i = 0; i < chars.size();) {
+        char& current = chars[i++];
+        int accum = 1;
+        while (i < chars.size() && chars[i] == current) {
+            ++accum; ++i;
+        }
+        chars[charsIdx++] = current;
+        if (accum > 1)
+            for (const char c : to_string(accum))
+                chars[charsIdx++] = c;
+    }
+    return charsIdx;
+}
+
+#ifdef _443
+int main(int argc, char** argv) {
+    while (true) {
+        int n; cin >> n;
+        vector<char> chars(n);
+        for (char& c : chars) cin >> c;
+        int compressed = compress(chars);
+        cout << compressed << "\n[ " << chars[0];
+        for (int i = 1; i < compressed; ++i)
+            cout << ", " << chars[i];
+        cout << " ]\n";
     }
     return 0;
 }
